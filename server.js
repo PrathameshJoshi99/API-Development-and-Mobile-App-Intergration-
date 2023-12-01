1cc0b68f9b35571db23f7e4a15d25e0afedb0300
Prathamesh Joshi <118970122+PrathameshJoshi99@users.noreply.github.com>
2023-12-01 20:37:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -69,3 +69,183 @@ const resolvers = {
     },
   },
 };
+
+
+// Extend the GraphQL schema with CRUD operations for tournaments
+const typeDefs = gql`
+  type Query {
+    hello: String
+    getTournament(id: ID!): Tournament
+  }
+
+  type Mutation {
+    generateRoomID: String
+    createTournament(name: String!): Tournament
+    updateTournament(id: ID!, name: String!): Tournament
+    deleteTournament(id: ID!): Boolean
+  }
+
+  type Tournament {
+    id: ID!
+    name: String!
+  }
+`;
+
+// Implement resolvers for CRUD operations
+const resolvers = {
+  Query: {
+    hello: () => 'Hello, World!',
+    getTournament: async (_, { id }, { pool }) => {
+      const client = await pool.connect();
+
+      try {
+        const result = await client.query('SELECT * FROM tournaments WHERE id = $1', [id]);
+        return result.rows[0];
+      } finally {
+        client.release();
+      }
+    },
+  },
+  Mutation: {
+    generateRoomID: async (_, __, { pool }) => {
+      // ... (same as previous code)
+    },
+    createTournament: async (_, { name }, { pool }) => {
+      const client = await pool.connect();
+
+      try {
+        const result = await client.query('INSERT INTO tournaments (name) VALUES ($1) RETURNING *', [name]);
+        return result.rows[0];
+      } finally {
+        client.release();
+      }
+    },
+    updateTournament: async (_, { id, name }, { pool }) => {
+      const client = await pool.connect();
+
+      try {
+        const result = await client.query('UPDATE tournaments SET name = $1 WHERE id = $2 RETURNING *', [name, id]);
+        return result.rows[0];
+      } finally {
+        client.release();
+      }
+    },
+    deleteTournament: async (_, { id }, { pool }) => {
+      const client = await pool.connect();
+
+      try {
+        await client.query('DELETE FROM tournaments WHERE id = $1', [id]);
+        return true;
+      } finally {
+        client.release();
+      }
+    },
+  },
+};
+
+// Extend the GraphQL schema with a type for the lobby
+const typeDefs = gql`
+  type Query {
+    hello: String
+    getTournament(id: ID!): Tournament
+    getLobby: [Player]
+  }
+
+  type Player {
+    id: ID!
+    username: String!
+  }
+
+  type Mutation {
+    generateRoomID: String
+    createTournament(name: String!): Tournament
+    updateTournament(id: ID!, name: String!): Tournament
+    deleteTournament(id: ID!): Boolean
+    joinLobby(username: String!): Player
+  }
+`;
+
+// Implement resolvers for lobby-related operations
+const resolvers = {
+  Query: {
+    hello: () => 'Hello, World!',
+    getTournament: async (_, { id }, { pool }) => {
+      // ... (same as previous code)
+    },
+    getLobby: async (_, __, { pool }) => {
+      const client = await pool.connect();
+
+      try {
+        const result = await client.query('SELECT * FROM players');
+        return result.rows;
+      } finally {
+        client.release();
+      }
+    },
+  },
+  Mutation: {
+    generateRoomID: async (_, __, { pool }) => {
+      // ... (same as previous code)
+    },
+    createTournament: async (_, { name }, { pool }) => {
+      // ... (same as previous code)
+    },
+    updateTournament: async (_, { id, name }, { pool }) => {
+      // ... (same as previous code)
+    },
+    deleteTournament: async (_, { id }, { pool }) => {
+      // ... (same as previous code)
+    },
+    joinLobby: async (_, { username }, { pool }) => {
+      const client = await pool.connect();
+
+      try {
+        const result = await client.query('INSERT INTO players (username) VALUES ($1) RETURNING *', [username]);
+        return result.rows[0];
+      } finally {
+        client.release();
+      }
+    },
+  },
+};
+
+
+const { PubSub } = require('apollo-server');
+
+const pubsub = new PubSub();
+
+// Extend the GraphQL schema with a subscription for tournament updates
+const typeDefs = gql`
+  type Query {
+    hello: String
+    getTournament(id: ID!): Tournament
+    getLobby: [Player]
+  }
+
+  type Mutation {
+    generateRoomID: String
+    createTournament(name: String!): Tournament
+    updateTournament(id: ID!, name: String!): Tournament
+    deleteTournament(id: ID!): Boolean
+    joinLobby(username: String!): Player
+  }
+
+  type Subscription {
+    tournamentUpdated: Tournament
+  }
+`;
+
+// Implement resolvers for subscriptions
+const resolvers = {
+  Query: {
+    // ... (same as previous code)
+  },
+  Mutation: {
+    // ... (same as previous code)
+  },
+  Subscription: {
+    tournamentUpdated: {
+      subscribe: () => pubsub.asyncIterator(['TOURNAMENT_UPDATED']),
+    },
+  },
+};
