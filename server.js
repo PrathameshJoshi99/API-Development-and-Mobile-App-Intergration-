93a8b59e67c01e05f94766972584654931567074
Prathamesh Joshi <118970122+PrathameshJoshi99@users.noreply.github.com>
2023-12-01 20:36:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -37,3 +37,35 @@ const server = new ApolloServer({
 server.listen().then(({ url }) => {
   console.log(`Server ready at ${url}`);
 });
+// File: server.js (continued)
+
+// Add mutation for generating a unique room ID
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+
+  type Mutation {
+    generateRoomID: String
+  }
+`;
+
+// Add resolver for the generateRoomID mutation
+const resolvers = {
+  Query: {
+    hello: () => 'Hello, World!',
+  },
+  Mutation: {
+    generateRoomID: async (_, __, { pool }) => {
+      const client = await pool.connect();
+
+      try {
+        // Generate a unique room ID (for simplicity, using a timestamp)
+        const roomID = `room_${Date.now()}`;
+        return roomID;
+      } finally {
+        client.release();
+      }
+    },
+  },
+};
